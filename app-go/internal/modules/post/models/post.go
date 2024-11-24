5ee5d97fc5c0b058be6b9f846329adafd8e5487d
vembry <airstrip99@gmail.com>
2024-11-24 19:58:18 +0000

    chore: define posts fields

--- app-go/internal/modules/post/models/post.go
+++ app-go/internal/modules/post/models/post.go
@@ -1,9 +1,29 @@
 package models
 
-import "app-go/internal/models"
+import (
+	"app-go/internal/models"
+	"encoding/json"
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
 
 // Post is an entity that contain information of a post
-type Post struct{}
+type Post struct {
+	Id        ksuid.KSUID     `gorm:"column:id"`
+	Text      string          `gorm:"column:text"`
+	Image     json.RawMessage `gorm:"column:image"`
+	CreatedBy string          `gorm:"column:created_by"`
+	CreatedAt time.Time       `gorm:"column:created_at"`
+	UpdatedAt time.Time       `gorm:"column:updated_at"`
+	DeletedAt time.Time       `gorm:"column:deleted_at"`
+}
+
+// PostImage contain image stored
+type PostImage struct {
+	Original    string `json:"original"`
+	Transformed string `json:"transformed"`
+}
 
 type CreatePostArg struct {
 	Creator string      `json:"creator"`
