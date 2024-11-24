1d57e20a96c7f28588fbe97cdfdf74e8f4729ac8
vembry <airstrip99@gmail.com>
2024-11-24 07:25:04 +0000

    chore: map out sections on main.go

---
+++ app-go/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"app-go/server/http"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	// initializing pre-requisites
+	log.Println("starting server...")
+
+	// NOTE: add modules initialization here
+	// ...
+
+	// NOTE: add server initialization on the following
+	// ...
+
+	httpserver := http.New(":4000")
+
+	// server starts
+	httpserver.Start()
+
+	waitForExitSignal()
+
+	// shutdown starts
+	log.Println("shutting down server...")
+
+	// NOTE: add shutdown handler on the following
+	// ...
+
+	httpserver.Stop() // stopping http server
+
+	log.Println("server stopped")
+}
+
+// waitForExitSignal is to awaits incoming interrupt signal
+// sent to the service
+func waitForExitSignal() os.Signal {
+	log.Printf("awaiting exit signal...")
+	ch := make(chan os.Signal, 4)
+	signal.Notify(
+		ch,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		syscall.SIGTERM,
+		syscall.SIGTSTP,
+	)
+
+	return <-ch
+}
